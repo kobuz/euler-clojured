789c9d9dc9ec00064b4e693e9fa1353c8d41dc26
kobuz <kobbuz@gmail.com>
2014-02-16 10:38:25 +0100

    lein empty app

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to euler-clojured
+
+TODO: write [great documentation](http://jacobian.org/writing/great-documentation/what-to-write/)
---
+++ test/euler_clojured/core_test.clj
@@ -0,0 +1,7 @@
+(ns euler-clojured.core-test
+  (:require [clojure.test :refer :all]
+            [euler-clojured.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
